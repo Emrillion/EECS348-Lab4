a4b49c4a48929617f147899ebc7c1689659619d4
Emrillion <simeongeib@ku.edu>
2026-02-26 10:13:45 -0600

    Actually add the rest of the functions

--- temperatureConversion.c
+++ temperatureConversion.c
@@ -17,6 +17,14 @@ float kelvin_to_celsius(float kelvin) {
     return kelvin - 273.15;
 }
 
+float fahrenheit_to_kelvin(float fahrenheit) {
+    return (5.0 / 9.0)*(fahrenheit - 32.0) + 273.15;
+}
+
+float kelvin_to_fahrenheit(float kelvin) {
+    return (9.0 / 5.0)*(kelvin - 273.15) + 32.0;
+}
+
 int TempConversionMain() {
     float temp;
     printf("Enter a temperature value:");
