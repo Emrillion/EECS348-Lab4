4220a31f62d4e8a2f1fc00f638986f21149c33d1
Emrillion <simeongeib@ku.edu>
2026-02-26 10:10:12 -0600

    Lay framework for temp conversion

--- temperatureConversion.c
+++ temperatureConversion.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+// A program to convert temperatures between various scales
 
-int TempConversion() {
+float celsius_to_fahrenheit(float c) {
+    return (9.0 / 5.0)*c + 32.0;
+}
+
+
+
+
+int TempConversionMain() {
+    float temp;
+    printf("Enter a temperature value:");
+    scanf("%f", &temp);
+
+
     return 0;
 }
